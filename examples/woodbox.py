a54744f2e84cf36819e3f9e933850daab5a73f4c
Stephen Eisenhauer <bhs2007@gmail.com>
2013-01-27 23:11:10 -0600

    Fix syntax errors in woodbox example map

--- examples/woodbox.py
+++ examples/woodbox.py
@@ -6,7 +6,7 @@ m = vmf.ValveMap()
 
 # Set up brushes for our walls
 walls = []
-for i in range(1):
+for i in range(6):
     walls.append(brush.Solid())
 
 # Floor
@@ -42,19 +42,19 @@ walls[0].children.extend(sides)
 # Wall 1
 sides = []
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 -448 512),
-    types.Vertex(512 -448 512),
-    types.Vertex(512 -512 512))))
+    types.Vertex(-512, -448, 512),
+    types.Vertex(512, -448, 512),
+    types.Vertex(512, -512, 512))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 -512 -512),
-    types.Vertex(512 -512 -512),
-    types.Vertex(512 -448 -512))))
+    types.Vertex(-512, -512, -512),
+    types.Vertex(512, -512, -512),
+    types.Vertex(512, -448, -512))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 -448 512),
+    types.Vertex(-512, -448, 512),
     types.Vertex(-512, -512, 512),
     types.Vertex(-512, -512, -512))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(512 -448 -512),
+    types.Vertex(512, -448, -512),
     types.Vertex(512, -512, -512),
     types.Vertex(512, -512, 512))))
 sides.append(brush.Side(types.Plane(
@@ -76,25 +76,25 @@ sides.append(brush.Side(types.Plane(
     types.Vertex(512, 512, 512),
     types.Vertex(512, 448, 512))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 448 -512),
-    types.Vertex(512 448 -512),
-    types.Vertex(512 512 -512))))
+    types.Vertex(-512, 448, -512),
+    types.Vertex(512, 448, -512),
+    types.Vertex(512, 512, -512))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 512 512),
-    types.Vertex(-512 448 512),
-    types.Vertex(-512 448 -512))))
+    types.Vertex(-512, 512, 512),
+    types.Vertex(-512, 448, 512),
+    types.Vertex(-512, 448, -512))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(512 512 -512),
-    types.Vertex(512 448 -512),
-    types.Vertex(512 448 512))))
+    types.Vertex(512, 512, -512),
+    types.Vertex(512, 448, -512),
+    types.Vertex(512, 448, 512))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(512 512 512),
-    types.Vertex(-512 512 512),
-    types.Vertex(-512 512 -512))))
+    types.Vertex(512, 512, 512),
+    types.Vertex(-512, 512, 512),
+    types.Vertex(-512, 512, -512))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(512 448 -512),
-    types.Vertex(-512 448 -512),
-    types.Vertex(-512 448 512))))
+    types.Vertex(512, 448, -512),
+    types.Vertex(-512, 448, -512),
+    types.Vertex(-512, 448, 512))))
 for side in sides:
     side.material = 'PL_BARNBLITZ/WOODWALL_YELLOWWORN002'
 walls[2].children.extend(sides)
@@ -102,29 +102,29 @@ walls[2].children.extend(sides)
 # Wall 3
 sides = []
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 448 448),
-    types.Vertex(-448 448 448),
-    types.Vertex(-448 -448 448))))
+    types.Vertex(-512, 448, 448),
+    types.Vertex(-448, 448, 448),
+    types.Vertex(-448, -448, 448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 -448 -448),
-    types.Vertex(-448 -448 -448),
-    types.Vertex(-448 448 -448))))
+    types.Vertex(-512, -448, -448),
+    types.Vertex(-448, -448, -448),
+    types.Vertex(-448, 448, -448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 448 448),
-    types.Vertex(-512 -448 448),
-    types.Vertex(-512 -448 -448))))
+    types.Vertex(-512, 448, 448),
+    types.Vertex(-512, -448, 448),
+    types.Vertex(-512, -448, -448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-448 448 -448),
-    types.Vertex(-448 -448 -448),
-    types.Vertex(-448 -448 448))))
+    types.Vertex(-448, 448, -448),
+    types.Vertex(-448, -448, -448),
+    types.Vertex(-448, -448, 448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-448 448 448),
-    types.Vertex(-512 448 448),
-    types.Vertex(-512 448 -448))))
+    types.Vertex(-448, 448, 448),
+    types.Vertex(-512, 448, 448),
+    types.Vertex(-512, 448, -448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-448 -448 -448),
-    types.Vertex(-512 -448 -448),
-    types.Vertex(-512 -448 448))))
+    types.Vertex(-448, -448, -448),
+    types.Vertex(-512, -448, -448),
+    types.Vertex(-512, -448, 448))))
 for side in sides:
     side.material = 'PL_BARNBLITZ/WOODWALL_YELLOWWORN002'
 walls[3].children.extend(sides)
@@ -132,29 +132,29 @@ walls[3].children.extend(sides)
 # Wall 4
 sides = []
 sides.append(brush.Side(types.Plane(
-    types.Vertex(448 448 448),
-    types.Vertex(512 448 448),
-    types.Vertex(512 -448 448))))
+    types.Vertex(448, 448, 448),
+    types.Vertex(512, 448, 448),
+    types.Vertex(512, -448, 448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(448 -448 -448),
-    types.Vertex(512 -448 -448),
-    types.Vertex(512 448 -448))))
+    types.Vertex(448, -448, -448),
+    types.Vertex(512, -448, -448),
+    types.Vertex(512, 448, -448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(448 448 448),
-    types.Vertex(448 -448 448),
-    types.Vertex(448 -448 -448))))
+    types.Vertex(448, 448, 448),
+    types.Vertex(448, -448, 448),
+    types.Vertex(448, -448, -448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(512 448 -448),
-    types.Vertex(512 -448 -448),
-    types.Vertex(512 -448 448))))
+    types.Vertex(512, 448, -448),
+    types.Vertex(512, -448, -448),
+    types.Vertex(512, -448, 448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(512 448 448),
-    types.Vertex(448 448 448),
-    types.Vertex(448 448 -448))))
+    types.Vertex(512, 448, 448),
+    types.Vertex(448, 448, 448),
+    types.Vertex(448, 448, -448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(512 -448 -448),
-    types.Vertex(448 -448 -448),
-    types.Vertex(448 -448 448))))
+    types.Vertex(512, -448, -448),
+    types.Vertex(448, -448, -448),
+    types.Vertex(448, -448, 448))))
 for side in sides:
     side.material = 'PL_BARNBLITZ/WOODWALL_YELLOWWORN002'
 walls[4].children.extend(sides)
@@ -162,29 +162,29 @@ walls[4].children.extend(sides)
 # Wall 5
 sides = []
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 512 512),
-    types.Vertex(512 512 512),
-    types.Vertex(512 -512 512))))
+    types.Vertex(-512, 512, 512),
+    types.Vertex(512, 512, 512),
+    types.Vertex(512, -512, 512))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 -512 448),
-    types.Vertex(512 -512 448),
-    types.Vertex(512 512 448))))
+    types.Vertex(-512, -512, 448),
+    types.Vertex(512, -512, 448),
+    types.Vertex(512, 512, 448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(-512 512 512),
-    types.Vertex(-512 -512 512),
-    types.Vertex(-512 -512 448))))
+    types.Vertex(-512, 512, 512),
+    types.Vertex(-512, -512, 512),
+    types.Vertex(-512, -512, 448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(512 512 448),
-    types.Vertex(512 -512 448),
-    types.Vertex(512 -512 512))))
+    types.Vertex(512, 512, 448),
+    types.Vertex(512, -512, 448),
+    types.Vertex(512, -512, 512))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(512 512 512),
-    types.Vertex(-512 512 512),
-    types.Vertex(-512 512 448))))
+    types.Vertex(512, 512, 512),
+    types.Vertex(-512, 512, 512),
+    types.Vertex(-512, 512, 448))))
 sides.append(brush.Side(types.Plane(
-    types.Vertex(512 -512 448),
-    types.Vertex(-512 -512 448),
-    types.Vertex(-512 -512 512))))
+    types.Vertex(512, -512, 448),
+    types.Vertex(-512, -512, 448),
+    types.Vertex(-512, -512, 512))))
 for side in sides:
     side.material = 'PL_BARNBLITZ/WOODWALL_YELLOWWORN002'
 walls[5].children.extend(sides)
