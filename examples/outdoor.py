c76de000ace72fa6282e55d8c30dc54b86585549
Stephen Eisenhauer <bhs2007@gmail.com>
2013-02-11 01:39:05 -0600

    More work on outdoor example map. Should be getting closer to what we want.

--- examples/outdoor.py
+++ examples/outdoor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Example map generator: Outdoor
 
 This script demonstrates vmflib by generating a map with a 2D skybox and
@@ -11,33 +11,41 @@ from vmf.tools import Block
 
 m = vmf.ValveMap()
 
-walls = []
-
 # Floor
-floor = Block(Vertex(0, 0, -512), (1024, 1024, 64))
+floor = Block(Vertex(0, 0, -512), (1024, 1024, 64), 'nature/dirtfloor012a')
 
 # Ceiling
 ceiling = Block(Vertex(0, 0, 512), (1024, 1024, 64))
 ceiling.set_material('tools/toolsskybox2d')
 
+skywalls = []
+# Left upper wall
+skywalls.append(Block(Vertex(-512, 0, 256), (64, 1024, 512)))
+# Right upper wall
+skywalls.append(Block(Vertex(512, 0, 256), (64, 1024, 512)))
+# Forward upper wall
+skywalls.append(Block(Vertex(0, 512, 256), (1024, 64, 512)))
+# Rear upper wall
+skywalls.append(Block(Vertex(0, -512, 256), (1024, 64, 512)))
+for wall in skywalls:
+    wall.set_material('tools/toolsskybox2d')
+
+walls = []
 # Left wall
-walls.append(Block(Vertex(-512, 0, 0), (64, 1024, 1024)))
-
+walls.append(Block(Vertex(-512, 0, -256), (64, 1024, 512)))
 # Right wall
-walls.append(Block(Vertex(512, 0, 0), (64, 1024, 1024)))
-
+walls.append(Block(Vertex(512, 0, -256), (64, 1024, 512)))
 # Forward wall
-walls.append(Block(Vertex(0, 512, 0), (1024, 64, 1024)))
-
+walls.append(Block(Vertex(0, 512, -256), (1024, 64, 512)))
 # Rear wall
-walls.append(Block(Vertex(0, -512, 0), (1024, 64, 1024)))
-
+walls.append(Block(Vertex(0, -512, -256), (1024, 64, 512)))
 # Set each wall's material
 for wall in walls:
     wall.set_material('PL_BARNBLITZ/WOODWALL_YELLOWWORN002')
 
 # Add walls to world geometry
 m.world.children.extend(walls)
+m.world.children.extend(skywalls)
 m.world.children.extend([floor, ceiling])
 
 # TODO: Define a playerspawn entity
