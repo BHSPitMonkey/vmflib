5da0ebec078c76e79331ccd733a76ba876fc393e
Stephen Eisenhauer <bhs2007@gmail.com>
2013-02-27 18:53:12 -0600

    Some more work on build tool (#6)

--- tools/buildbsp.py
+++ tools/buildbsp.py
@@ -21,17 +21,21 @@ import sys
 import subprocess
 
 games = {
-    'tf2': '',
-    'css': '',
-    'hl2': ''
+    'tf2': {'id': 440},
+    'css': {'id': 240},
+    'hl2': {'id': 220},
+    'gm': {'id': 4000}
     }
 
 def _make_arg_parser():
     parser = argparse.ArgumentParser(description='Build a VMF map.')
     parser.add_argument('map')
-    parser.add_argument('-g', '--game', default='tf2', choices=games.keys())
-    parser.add_argument('--no-run')
-    parser.add_argument('--no-install')
+    parser.add_argument('-g', '--game', default='tf2', choices=games.keys(),
+        help="selects which game to use")
+    parser.add_argument('--no-run',
+        help="don't run the game after building/installing")
+    parser.add_argument('--no-install',
+        help="don't install (or run) the map after building")
 
     return parser
 
@@ -41,9 +45,20 @@ if __name__ == '__main__':
     #opts, args = arg_parser.parse_args()
     
     if sys.platform.startswith('win32'):
-        # TODO:
+        # Environmental scan
         # - Figure out paths we'll need (maybe detect where steam lives?)
+        # TODO
+        
+        # Run the SDK tools
         # - Use subprocess to call the tools
-        print("I don't work yet!")
+        # TODO
+        
+        # Install the map to the game's map directory (unless --no-install)
+        # TODO
+        
+        # Launch the game (unless --no-run or --no-install)
+        # TODO
+        # steam://run/{id} -dev -console -allowdebug +map {mapname}
+        print("I don't work yet!")        
     else:
         raise OSError('Your OS is not supported yet!')
