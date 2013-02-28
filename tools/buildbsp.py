6c27abbd24f5fdb99ab2aa0e5d87a5c5f45d934b
Stephen Eisenhauer <bhs2007@gmail.com>
2013-02-28 01:20:21 -0600

    Additions to build tool (#6)

--- tools/buildbsp.py
+++ tools/buildbsp.py
@@ -18,7 +18,9 @@ Finished use should resemble something like:
 
 import argparse
 import sys
+import os
 import subprocess
+import webbrowser
 
 games = {
     'tf2': {'id': 440},
@@ -32,33 +34,48 @@ def _make_arg_parser():
     parser.add_argument('map')
     parser.add_argument('-g', '--game', default='tf2', choices=games.keys(),
         help="selects which game to use")
-    parser.add_argument('--no-run',
+    parser.add_argument('--no-run', action="store_true",
         help="don't run the game after building/installing")
-    parser.add_argument('--no-install',
+    parser.add_argument('--no-install', action="store_true",
         help="don't install (or run) the map after building")
+    parser.add_argument('--steamapps',
+        help="location of your user's steamapps subfolder")
 
     return parser
 
 if __name__ == '__main__':
     parser = _make_arg_parser()
     args = parser.parse_args()
-    #opts, args = arg_parser.parse_args()
+    vmf_file = os.path.abspath(args.map)
     
     if sys.platform.startswith('win32'):
+        # Define constants
+        games['tf2']['gamedir'] = 'team fortress 2\tf'
+
         # Environmental scan
         # - Figure out paths we'll need (maybe detect where steam lives?)
+        # Best I can figure out for now is accepting a path as an argument
+        steamapps = os.path.abspath(args.steamapps)
+        sdkbin = "%s\sourcesdk\bin\orangebox\bin" % steamapps
+        game = games[args.game]
+        gamedir = "%s\%s" % (steamapps, game['gamedir'])
         # TODO
         
         # Run the SDK tools
         # - Use subprocess to call the tools
         # TODO
+        vbsp_cmd = '"%s\vbsp.exe" -game "%s" "%s"' % (sdkbin, gamedir, vmf_file)
+        print(vbsp_cmd)
         
         # Install the map to the game's map directory (unless --no-install)
         # TODO
         
         # Launch the game (unless --no-run or --no-install)
-        # TODO
-        # steam://run/{id} -dev -console -allowdebug +map {mapname}
+        if not args.no_run and not args.no_install:
+            webbrowser.open("steam://run/%d//-dev -console -allowdebug +map %s" % (game['id'], "outdoor"))
+        else:
+            print("Not launching game")
+
         print("I don't work yet!")        
     else:
         raise OSError('Your OS is not supported yet!')
