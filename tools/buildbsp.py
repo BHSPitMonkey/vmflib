cdd3f3a02f928063698337ada29b61a7e90287ab
Stephen Eisenhauer <bhs2007@gmail.com>
2013-03-02 23:55:56 -0600

    Fix cygwin support in build tool and add error handling.
    Linux/wine support still unresolved. Fixes #6.

--- tools/buildbsp.py
+++ tools/buildbsp.py
@@ -2,10 +2,11 @@
 """
 
 Utility for building a map using installed Source SDK tools.
+Call with -h or --help to see usage information.
 
-Finished use should resemble something like:
+Examples:
 
-  # Creates/installs/runs standard .bsp in same dir
+  # Creates/installs/runs .bsp in same dir
   python buildbsp.py --game tf2 mymap.vmf
   
   # Creates/installs .bsp but does not run
@@ -99,32 +100,42 @@ if __name__ == '__main__':
 
         # Run the SDK tools
         vbsp_exe = os.path.join(sdkbin, "vbsp.exe")
-        vbsp_cmd = '"%s" -game "%s" "%s"' % (vbsp_exe, gamedir, mappath)
-        print(vbsp_cmd)
-        subprocess.call(vbsp_cmd)
-        
+        code = subprocess.call([vbsp_exe, '-game', gamedir, mappath])
+        print("VBSP finished with status %s." % code)
+
+        if code == 1:
+            print("Looks like SteamService isn't working. Try reopening Steam.")
+            exit(code)
+        elif code == -11:
+            print("Looks like you might have gotten the 'material not found' " +
+                "error messages. Try signing into Steam, or restarting it " +
+                "and signing in.")
+            exit(code)
+        elif code != 0:
+            print("Looks like VBSP crashed, but I'm not sure why.")
+            exit(code)
+
         vvis_exe = os.path.join(sdkbin, "vvis.exe")
-        opts = ''
+        opts = [vvis_exe]
         if args.fast:
-            opts += '-fast '
-        vvis_cmd = '"%s" %s-game "%s" "%s"' % (vvis_exe, opts, gamedir, mappath)
-        print(vvis_cmd)
-        subprocess.call(vvis_cmd)
-        
+            opts.append('-fast')
+        opts.extend(['-game', gamedir, mappath])
+        subprocess.call(opts)
+
         vrad_exe = os.path.join(sdkbin, "vrad.exe")
-        opts = ''
+        opts = [vrad_exe]
         if args.fast:
-            opts += '-bounce 2 -noextra '
+            opts.extend(['-bounce', '2', '-noextra'])
         if args.hdr:
-            opts += '-both '
+            opts.append('-both')
         if args.hdr and args.final:
-            opts += '-final '
-        vrad_cmd = '"%s" %s-game "%s" "%s"' % (vrad_exe, opts, gamedir, mappath)
-        print(vrad_cmd)
-        subprocess.call(vrad_cmd)
+            opts.append('-final')
+        opts.extend(['-game', gamedir, mappath])
+        subprocess.call(opts)
 
         # Install the map to the game's map directory (unless --no-install)
         if not args.no_install:
+            print("Copying map %s to %s" % (mapname, mapsdir))
             shutil.copy(bsp_file, mapsdir)
         else:
             print("Not installing map")
@@ -135,12 +146,17 @@ if __name__ == '__main__':
             run_url = "steam://run/%d//%s" % (game['id'], params)
             print(run_url)
             webbrowser.open(run_url)
+            if cygwin:
+                print("\nYou're running cygwin, so I can't launch the game for you.")
+                print("Double-click the URL above, right-click, and click 'Open'.")
+                print("Or paste the URL above into the Windows 'Run...' dialog.")
+                print("Or, just run 'map %s' in the in-game console." % mapname)
         else:
             print("Not launching game")
     elif linux:
         # Define constants
         games['tf2']['gamedir'] = os.path.join("Team Fortress 2", "tf")
-        
+
         # Environmental scan
         sourcesdk = os.path.abspath(args.sourcesdk)
         steamapps = os.path.join(sourcesdk, '..')
@@ -148,20 +164,20 @@ if __name__ == '__main__':
         sdkbin = os.path.join(sourcesdk, "bin", "orangebox", "bin")
         game = games[args.game]
         gamedir = os.path.join(steamapps, game['gamedir'])
-        
+
         # Use native maps directory instead of wine's
         mapsdir = os.path.join('~', '.steam', 'steam', 'SteamApps', username, game['gamedir'], "maps")
-        
+
         # Change working directory first because VBSP is dumb
         os.chdir(os.path.join(sourcesdk, 'bin', 'orangebox'))
-        
+
         # Environment to use with wine calls
         env = os.environ.copy()
         env['WINEPREFIX'] = "~/.winesteam"
-        
+
         # Run the SDK tools
         #TODO
-        return
+        exit(-1)
         
         # Install the map to the game's map directory (unless --no-install)
         if not args.no_install:
