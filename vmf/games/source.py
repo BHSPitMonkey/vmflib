b66ad576230fb7c96a8f5c6c7b6af8a8e4c8d0b5
Stephen Eisenhauer <bhs2007@gmail.com>
2013-04-09 23:20:15 -0500

    Add module for Source Engine game logic entities

---
+++ vmf/games/source.py
@@ -0,0 +1,20 @@
+"""
+
+Helper classes for creating maps in any Source Engine game.
+
+"""
+
+from vmf.vmf import Entity
+from vmf.types import Origin
+
+class LogicAuto(Entity):
+
+    """Sets up certain game logic. Fires some useful map events.
+    
+    https://developer.valvesoftware.com/wiki/Logic_auto
+    
+    """
+
+    def __init__(self):
+        Entity.__init__(self, "logic_auto")
+        self.origin = Origin()
