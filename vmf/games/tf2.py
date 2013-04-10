8cdfe6e6bcdddb1285ac0c9d8b26cd9274a89d1f
Stephen Eisenhauer <bhs2007@gmail.com>
2013-04-10 00:13:48 -0500

    TF2 Koth logic now sets more of itself up

--- vmf/games/tf2.py
+++ vmf/games/tf2.py
@@ -4,8 +4,8 @@ Helper classes specific to creating maps for Team Fortress 2.
 
 """
 
-from vmf.vmf import Entity
-from vmf.types import Bool, Origin
+from vmf.vmf import Entity, Connections
+from vmf.types import Bool, Origin, Output
 
 class GameRules(Entity):
     
@@ -30,11 +30,14 @@ class LogicKoth(Entity):
     
     """Activates King of the Hill mode.
     
+    logic_auto should be the map's source.LogicAuto object
+    gamerules should be the map's tf2.GameRules object
+    
     https://developer.valvesoftware.com/wiki/Tf_logic_koth
     
     """
     
-    def __init__(self):
+    def __init__(self, logic_auto, gamerules, respawn_wave_time=6):
         Entity.__init__(self, "tf_logic_koth")
         self.origin = "0 0 0"
         self.targetname = "tf_logic_koth"
@@ -43,7 +46,22 @@ class LogicKoth(Entity):
         self.unlock_point = 30   # Seconds after which to enable capping
 
         self.auto_properties += ['timer_length', 'unlock_point']
-
+        
+        # Set up necessary connections
+        if logic_auto and gamerules:
+            gr = gamerules.targetname
+            c = Connections()
+            c.children.extend([
+                Output("OnMapSpawn", gr, "SetBlueTeamGoalString",
+                    "#koth_setup_goal"),
+                Output("OnMapSpawn", gr, "SetRedTeamGoalString",
+                    "#koth_setup_goal"),
+                Output("OnMapSpawn", gr, "SetRedTeamRespawnWaveTime",
+                    respawn_wave_time),
+                Output("OnMapSpawn", gr, "SetBlueTeamRespawnWaveTime",
+                    respawn_wave_time)
+            ])
+            logic_auto.children.append(c)
 
 class SpawnPoint(Entity):
 
