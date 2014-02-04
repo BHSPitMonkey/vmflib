322757fcba98e05041ee8f416c8ffe847ca1fe64
Stephen Eisenhauer <bhs2007@gmail.com>
2014-02-04 02:03:54 -0600

    Tagging v0.1.1 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "vmflib",
-    version = "0.1",
+    version = "0.1.1",
     packages = find_packages(),
     scripts = ['tools/buildbsp.py'],
 
